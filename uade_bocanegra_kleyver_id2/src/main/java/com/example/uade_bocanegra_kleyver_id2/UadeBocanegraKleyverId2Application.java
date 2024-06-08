8bd9e51fbb99e1453b0d90d3969506f892b2d261
BocanegraKleyver <kbocanegracisneros@uade.edu.ar>
2024-06-07 22:30:32 -0300

    3: redis-server --port 6380 para inicializar Redis.

---
+++ uade_bocanegra_kleyver_id2/src/main/java/com/example/uade_bocanegra_kleyver_id2/UadeBocanegraKleyverId2Application.java
@@ -0,0 +1,13 @@
+package com.example.uade_bocanegra_kleyver_id2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UadeBocanegraKleyverId2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UadeBocanegraKleyverId2Application.class, args);
+	}
+
+}
---
+++ uade_bocanegra_kleyver_id2/src/test/java/com/example/uade_bocanegra_kleyver_id2/UadeBocanegraKleyverId2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uade_bocanegra_kleyver_id2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UadeBocanegraKleyverId2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
